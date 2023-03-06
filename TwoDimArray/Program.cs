553035f77eb6538d80ddb4f3eb98b5cb8ebebd34
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-03-06 20:53:40 +0300

    Add task TwoDimArray

---
+++ TwoDimArray/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Random random = new Random();
+
+double [,] array = FillArray(4,4);
+ReadArray(array);
+
+double[,] FillArray(int row, int column)
+{
+  double[,] array = new double[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      array[i, j] = random.NextDouble()* 100 - 50;
+    }
+  }
+  return array;
+}
+
+void ReadArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      System.Console.Write("{0:f2} ", array[i, j]);
+    }
+    System.Console.WriteLine();
+  }
+}
